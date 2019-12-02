49be39020dab9d1994976c3e6a0948ac4bf7eb81
JohnyLex <49374834+JohnyLex@users.noreply.github.com>
2019-12-03 00:22:28 +0200

    Update valute_detector_keras.py

--- valute_detector_keras.py
+++ valute_detector_keras.py
@@ -12,6 +12,7 @@ data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 model = tensorflow.keras.models.load_model('keras_model.h5')
 opencv='opencv'
 values = {'val': opencv}
+cap = cv2.VideoCapture(0)
 t = string.Template("""
 ----------------Bine ati venit-----------------
 Acest program a fost creat in scopuri educative
@@ -68,30 +69,18 @@ if buttons in (None,'Registration'):
     [sg.Button(button_text='Capture'),sg.Button(button_text='Cancel'),sg.Text('Give name of capturing foto'),sg.Input(size=(20,1))]
     ]
     windo = sg.Window(' OpenCV Integration-Foto by Soft', [[sg.Image(filename='', key='image')],], location=(800,400)).layout(cv_integration)
-    cap = cv2.VideoCapture(0)
-    check, frame = cap.read()
+    
     
     
     while True:
+        check, frame = cap.read()
         event, values = windo.Read(timeout=20, timeout_key='timeout') 
         foto_name=[values[0]]
+        
+        
         if event in (None,'Capture'):
             cv2.imwrite(filename=f'{foto_name}.jpg', img=frame)
             cap.release()
-            img_new = cv2.imread(f'{foto_name}.jpg', cv2.IMREAD_GRAYSCALE)
-            img_new = cv2.imshow("Captured Image", img_new)
-            cv2.waitKey(1650)
-            cv2.destroyAllWindows()
-            print("Processing image...")
-            img_ = cv2.imread(f'{foto_name}.jpg', cv2.IMREAD_ANYCOLOR)
-            print("Converting RGB image to grayscale...")
-            gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-            print("Converted RGB image to grayscale...")
-            print("Resizing image to 28x28 scale...")
-            img_ = cv2.resize(gray,(28,28))
-            print("Resized...")
-            img_resized = cv2.imwrite(filename=f'foto_name_.jpg', img=img_)
-            print("Image saved!")
             image = Image.open(f'{foto_name}.jpg')
             # Make sure to resize all images to 224, 224 otherwise they won't fit in the array
             image = image.resize((224, 224))
@@ -107,6 +96,7 @@ if buttons in (None,'Registration'):
         if event in (None,'Cancel'):
             print("Turning off camera.")
             cap.release()
+            cv.destroyAllWindows()
             print("Camera off.")
             print("Program ended.")
             cv2.destroyAllWindows()
@@ -114,7 +104,8 @@ if buttons in (None,'Registration'):
 
 if buttons in (None, 'Cance'):
     exit()
-
+cap.release()
+cv2.destroyAllWindows()
 
 
 
